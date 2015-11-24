1f7fe7235c3142eedb5c4810a38352890a2d8d82
Jake Greene <jake.greene@razorfish.com>
2015-11-23 20:11:04 -0500

    Sized Parsers
    Each parser knows the size of the expected input. This
    cleans-up the parsing logic.

--- src/main/scala/ca/jakegreene/csv/Parser.scala
+++ src/main/scala/ca/jakegreene/csv/Parser.scala
@@ -5,6 +5,7 @@ import shapeless._
 
 trait Parser[T] {
   def apply(cells: Seq[String]): Parser.ParseResult[T]
+  def size: Int
 }
 
 /**
@@ -12,30 +13,35 @@ trait Parser[T] {
  */
 object Parser {
   
-  type CellResult[T] = (T, Seq[String])
-  type ParseResult[T] = Either[String, CellResult[T]]
+  type ParseResult[T] = Either[String, T]
 
-  def parse[T](s: String)(implicit parser: Parser[T]): Either[String, T] = {
+  def parse[T](s: String)(implicit parser: Parser[T]): ParseResult[T] = {
     val cells = s.split(",").toSeq
-    parser(cells) match {
-      case Right((parsed, rest)) if rest.isEmpty => Right(parsed)
-      case Right((_, rest)) => Left(s"Attempted to parse but left with ${rest.mkString(",")}")
-      case Left(message) => Left(message)
-    }
+    parser(cells)
   }
 
-  def instance[T](p: Seq[String] => ParseResult[T]): Parser[T] = new Parser[T] {
-    def apply(cells: Seq[String]): ParseResult[T] = p(cells)
+  def instance[T](s: Int)(p: Seq[String] => ParseResult[T]): Parser[T] = new Parser[T] {
+    def apply(cells: Seq[String]): ParseResult[T] = {
+      if (cells.length == s) {
+        p(cells)
+      } else {
+        Left(s"Input size [${cells.length}] does not match parser expected size [$s]")
+      }
+    }
+    def size = s
   }
-  
+
   def headInstance[T](p: String => Option[T])(failure: String => String): Parser[T] = new Parser[T] {
     def apply(cells: Seq[String]): ParseResult[T] = {
-      val maybeResult = for {
-        head <- cells.headOption
-        parsed <- p(head)
-      } yield (parsed, cells.tail)
-      maybeResult.toRight(failure(cells.headOption.getOrElse("")))
+      cells match {
+        case head +: Nil =>
+          val maybeParsed = p(head)
+          maybeParsed.toRight(failure(head))
+        case _ =>
+          Left(failure(cells.mkString(", ")))
+      }
     }
+    def size = 1
   }
 
   implicit val stringParser = headInstance(Some(_))(s => s"Cannot parse [$s] to String")
@@ -45,17 +51,18 @@ object Parser {
    *  Currently accepts anything. This is due to the situation where an inner case class is not
    *  the last param of an outer case class.
    */
-  implicit val hnilParser: Parser[HNil] = instance(s => Right((HNil, s)))
+  implicit val hnilParser: Parser[HNil] = instance(0)(s => Right((HNil)))
 
   // CSV regex
   private[this] val NextCell = "^([^,]+)(?:,(.+))?$".r
 
   implicit def hconsParser[Head, Tail <: HList](implicit hp: Lazy[Parser[Head]], tp: Lazy[Parser[Tail]]): Parser[Head :: Tail] = {
-    instance { cells =>
+    instance(hp.value.size + tp.value.size) { cells =>
       // Compiler bug SI-7222 prevents this from being a for-comprehension
-      hp.value(cells).right.flatMap { case (head, rest) =>
-        tp.value(rest).right.map { case (tail, remaining) =>
-          (head :: tail, remaining)
+      val (headCells, tailCells) = cells.splitAt(hp.value.size)
+      hp.value(headCells).right.flatMap { case (head) =>
+        tp.value(tailCells).right.map { case (tail) =>
+          (head :: tail)
         }
       }
     }
@@ -65,9 +72,9 @@ object Parser {
    * A parser for a case class that has a parser for it's HList representation
    */
   implicit def caseClassParser[Case, Repr <: HList](implicit gen: Generic.Aux[Case, Repr], reprParser: Lazy[Parser[Repr]]): Parser[Case] = {
-    instance { cells => 
-      reprParser.value(cells).right.map { case (parsed, rest) =>
-        (gen.from(parsed), rest)
+    instance(reprParser.value.size) { cells =>
+      reprParser.value(cells).right.map { parsed =>
+        (gen.from(parsed))
       }
     }
   }
